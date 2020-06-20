7661e025c5a5cd6508aa1b4c76a441361f1fbdae
Jack-42 <jakob-robert@hotmail.de>
2020-06-21 00:47:41 +0200

    Init WebGL, draw black screen

--- public/src/app.js
+++ public/src/app.js
@@ -1,3 +1,11 @@
 function start() {
-    console.log("Hello World!")
+    const canvas = document.getElementById("gl-canvas");
+    const gl = canvas.getContext("webgl");
+    if (!gl) {
+        alert("Your browser does not support WebGL!");
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
